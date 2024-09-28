80266129b60455e13afd93412064d88ed0583ccb
Tomoki-Osaki <hantoumeiningenn@gmail.com>
2024-09-28 17:31:43 +0900

    made a func to create a dict to access all data by ID, trial, etc

--- re-analysis.py
+++ re-analysis.py
@@ -5,43 +5,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('04_RawData/20_omoiyari.csv')
-df
-
 def show_all(obj):
     for i in obj: print(i)
-
-show_all(df.columns)
-
-df_5 = df.query('type.str.contains("05")')
-df_10 = df.query('type.str.contains("10")')
-df_20 = df.query('type.str.contains("20")')
-
-tri_num_5 = list(set(df_5['trial']))
-tri_num_10 = list(set(df_10['trial']))
-tri_num_20 = list(set(df_20['trial']))
-
-df_5_tri = df_5.query("trial == @tri_num_5[0]")
-df_10_tri = df_10.query("trial == @tri_num_10[0]")
-df_20_tri = df_20.query("trial == @tri_num_20[0]")
-
-df_5_tri.myNextX.plot(); df_5_tri.myNextY.plot(); plt.show()
-df_10_tri.myNextX.plot(); df_10_tri.myNextY.plot(); plt.show()
-df_20_tri.myNextX.plot(); df_20_tri.myNextY.plot(); plt.show()
     
-def plot_traj(df):
-    for x, y in zip(df['myNextX'], df['myNextY']):
-        plt.scatter(x, y)
+def make_start_from_UL(df):
+    if "UR" in df["type"].values[0]: # (940, 30)
+        df["posX"] = df["myNextX"].map(lambda x: abs(x - 910))
+        df["posY"] = df["myNextY"]
+        
+    if "LR" in df["type"].values[0]: # (940, 940)
+        df["posX"] = df["myNextX"].map(lambda x: abs(x - 910))
+        df["posY"] = df["myNextY"].map(lambda y: abs(y - 910))
+        
+    if "LL" in df["type"].values[0]: # (30, 940)
+        df["posX"] = df["myNextX"]
+        df["posY"] = df["myNextY"].map(lambda y: abs(y - 910))
+
+    if "UL" in df["type"].values[0]: # (30, 30)
+        df["posX"] = df["myNextX"]
+        df["posY"] = df["myNextY"]
+
+    return df
+
+def make_dict_containing_all_info():
+    pd.options.mode.chained_assignment = None # otherwise there'll be many warnings
+    
+    exp_conditions = ["omoiyari", "urgent", "nonurgent", "control"]
+    df_all_participants = {}
+    for subjectID in range(1, 30):
+        df_all_participants[f"ID_{subjectID}"] = {}
+        
+    for condition in exp_conditions:
+        for i in range(1, 30):
+        
+            df = pd.read_csv(f'04_RawData/{i}_{condition}.csv')
+            
+            df_5 = df.query('type.str.contains("05")')
+            df_10 = df.query('type.str.contains("10")')
+            df_20 = df.query('type.str.contains("20")')
+            
+            tri_num_5 = list(set(df_5['trial']))
+            tri_num_10 = list(set(df_10['trial']))
+            tri_num_20 = list(set(df_20['trial']))
+            assert len(tri_num_5) == len(tri_num_10) == len(tri_num_20), "all trials must have same lengths"
+            
+            dfs_per_trials = {}
+            for j in range(len(tri_num_5)):
+                df_5_tri = df_5.query("trial == @tri_num_5[@j]")
+                df_5_tri = make_start_from_UL(df_5_tri)
+                df_10_tri = df_10.query("trial == @tri_num_10[@j]")
+                df_10_tri = make_start_from_UL(df_10_tri)
+                df_20_tri = df_20.query("trial == @tri_num_20[@j]")
+                df_20_tri = make_start_from_UL(df_20_tri)
+                
+                dfs_per_trials[f"agents5_tri{j+1}"] = df_5_tri
+                dfs_per_trials[f"agents10_tri{j+1}"] = df_10_tri
+                dfs_per_trials[f"agents20_tri{j+1}"] = df_20_tri
+        
+            df_all_participants[f"ID_{i}"][f"{condition}"] = dfs_per_trials
+
+    return df_all_participants
+
+df_all_participants = make_dict_containing_all_info()
+
+# df_id_conditions_NumOfAgents_Trialnumber
+df1omoiyari51 = df_all_participants["ID_1"]["omoiyari"]["agents5_tri1"]
+
+def plot_all_trials(ID, conditions, num_of_agents):
+    fig = plt.figure(figsize=(12, 8))
+    for trial in range(1, 9):
+        df = df_all_participants[f"ID_{ID}"][conditions][f"agents{num_of_agents}_tri{trial}"]
+        ax = fig.add_subplot(2, 4, trial)
+        for x, y in zip(df['posX'], df['posY']):
+            ax.scatter(x, y, color="blue", alpha=.5)
+        ax.set_title(f"tri{trial}")
+    plt.suptitle(f"ID_{ID}_{conditions}_agents{num_of_agents}")
     plt.show()
-    
-plot_traj(df_5_tri)
-plot_traj(df_10_tri)
-plot_traj(df_20_tri)
 
-p_o = np.array([0, 0])
-for i, (x, y) in enumerate(zip(df['myNextX'], df['myNextY'])):
-    a = np.array([x, y])
-    distance = np.linalg.norm(p_o - a)
-    plt.scatter(i, distance)
-plt.show()
+plot_all_trials(1, "omoiyari", 20)
+plot_all_trials(1, "urgent", 20)
 
