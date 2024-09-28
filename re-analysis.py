76858eac84f0a802e3ed215d151e11792a2757e7
Tomoki Osaki <hantoumeiningenn@gmail.com>
2024-09-28 12:48:36 +0900

    added some analysis

--- re-analysis.py
+++ re-analysis.py
@@ -1,4 +1,4 @@
-# control condition (no instructions for the moving behavior)
+# control, urgent, nonurgent, omoiyari
 import os
 os.chdir("C:/Users/ootmo/OneDrive/Desktop/修論/共同P_論文/松林さん/04_RawData")
 import pandas as pd
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('2_omoiyari.csv')
+df = pd.read_csv('20_omoiyari.csv')
 df
 
 def show_all(obj):
@@ -29,22 +29,6 @@ df_20_tri = df_20.query("trial == @tri_num_20[0]")
 df_5_tri.myNextX.plot(); df_5_tri.myNextY.plot(); plt.show()
 df_10_tri.myNextX.plot(); df_10_tri.myNextY.plot(); plt.show()
 df_20_tri.myNextX.plot(); df_20_tri.myNextY.plot(); plt.show()
-
-for i in range(len(tri_num_5)):
-    df_5_tri = df_5.query("trial == @tri_num_5[@i]")
-    df_5_tri.myNextX.plot(); df_5_tri.myNextY.plot() 
-    plt.show()
-    
-for i in range(len(tri_num_10)):
-    df_10_tri = df_10.query("trial == @tri_num_10[@i]")
-    df_10_tri.myNextX.plot(); df_10_tri.myNextY.plot() 
-    plt.show()
-    
-for i in range(len(tri_num_20)):
-    df_20_tri = df_20.query("trial == @tri_num_20[@i]")
-    df_20_tri.myNextX.plot(); df_20_tri.myNextY.plot() 
-    plt.show()
-    
     
 def plot_traj(df):
     for x, y in zip(df['myNextX'], df['myNextY']):
@@ -55,4 +39,16 @@ plot_traj(df_5_tri)
 plot_traj(df_10_tri)
 plot_traj(df_20_tri)
 
+p_o = np.array([0, 0])
+for i, (x, y) in enumerate(zip(df['myNextX'], df['myNextY'])):
+    a = np.array([x, y])
+    distance = np.linalg.norm(p_o - a)
+    plt.scatter(i, distance)
+plt.show()
+
+for i in range(1, 30):
+    os.rename(f'{i}_b.csv', f'{i}_practice.csv')
+    os.rename(f'{i}_i.csv', f'{i}_urgent.csv')
+    os.rename(f'{i}_o.csv', f'{i}_omoiyari.csv')
+    os.rename(f'{i}_s.csv', f'{i}_nonurgent.csv')
 
