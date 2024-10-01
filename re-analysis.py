4ae07e325c7e0c4b6fc6d9eb8ddeb562032554a9
Tomoki Osaki <hantoumeiningenn@gmail.com>
2024-10-01 11:13:00 +0900

    successfully debugged

--- re-analysis.py
+++ re-analysis.py
@@ -9,9 +9,7 @@ from gc import collect as gc
 
 df_all = mf.make_dict_containing_all_info(29)
 
-df = mf.make_all_debug(29)
-
-df_small = df_all["ID12"]["omoiyari"]["agents20_tri4"]
+df_small = df_all["ID3"]["omoiyari"]
 
 # df_id_conditions_NumOfAgents_Trialnumber
 # df1omoiyari51 = df_all_participants["ID1"]["omoiyari"]["agents5_tri1"]
@@ -22,30 +20,6 @@ tmp = df_small[cols]
 tmp.reset_index(drop=True, inplace=True)
 #tmp.drop(tmp.index[:3], inplace=True)
 tmp.reset_index(drop=True, inplace=True)
-
-def calc_ideal_positions(df):
-    posIdeal = df.apply(lambda df: mf.line_equation_to_goal(df["posX"], df["posY"]), axis=1)
-    idealX, idealY = [], []
-    for i, _ in enumerate(posIdeal):
-        idealX.append(posIdeal[i][0])
-        idealY.append(posIdeal[i][1])
-    df['idealX'] = idealX
-    df['idealY'] = idealY
-    
-    return df
-
-posIdeal = df_5_tri.apply(lambda df: mf.line_equation_to_goal(df["posX"], df["posY"]), axis=1)
-posIdeal.reset_index(drop=True, inplace=True)
-idealX, idealY = [], []
-for i, _ in enumerate(posIdeal):
-    idealX.append(posIdeal[i][0])
-    idealY.append(posIdeal[i][1])
-df_5_tri['idealX'] = idealX
-df_5_tri['idealY'] = idealY
-
-tmp = calc_ideal_positions(df_5_tri)
-
-tmp = calc_ideal_positions(tmp)
 
 dists = []
 for ix, iy, rx, ry in zip(tmp.idealX[:-1], tmp.idealY[:-1], tmp.posX[1:], tmp.posY[1:]):
