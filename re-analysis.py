37e360c9a3a3fb0d56a8d8f6005171664bb646f2
Tomoki-Osaki <hantoumeiningenn@gmail.com>
2024-10-02 16:49:59 +0900

    cleaned

--- re-analysis.py
+++ re-analysis.py
@@ -15,9 +15,3 @@ df_part = df_small["agents20_tri2"]
 mf.plot_traj_per_trials(df_all, 1, 'urgent', 5)
 mf.plot_traj_all_trials(df_all, 1, 'urgent', 5)
 mf.plot_traj_compare_conds(df_all, 1, 5)
-
-dists = []
-for i in range(len(df_part.index)):
-    other = np.array([df_part["other1NextX"][i], df_part["other1NextY"][i]])
-    own = np.array([df_part["myNextX"][i], df_part["myNextY"][i]])
-    dists.append(np.linalg.norm(other - own))
