004f3279160fc1c3d397486b3411516fbb27cfca
Tomoki Osaki <hantoumeiningenn@gmail.com>
2024-10-03 01:07:00 +0900

    deeper hierarchy

--- myfuncs.py
+++ myfuncs.py
@@ -1,25 +1,30 @@
+"""
+1.  show_all
+2.  make_start_from_UL
+3.  _calc_ideal_positions
+4.  add_cols_ideal_positions
+5.  calc_dist_real_ideal
+6.  drop_unnecessary_cols
+7.  _calc_distance
+8.  add_cols_dist_others
+9.  calc_closest_others
+10. preprocess
+11. make_empty_hierarchical_df
+12. make_dict_of_all_info
+13. make_df_trial
+13. plot_traj_per_trials
+14. plot_traj_compare_conds
+"""
+
+# %%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+import seaborn as sns
+sns.set_theme()
 from tqdm import tqdm
 from typing import Literal
-
-"""
-1. show_all
-2. make_start_from_UL
-3. _calc_ideal_positions
-4. add_cols_ideal_positions
-5. calc_dist_real_ideal
-6. drop_unnecessary_cols
-7. _calc_distance
-8. add_cols_dist_others
-9. calc_closest_others
-10. preprocess
-11. make_dict_containing_all_info
-12. plot_traj_per_trials
-13. plot_traj_compare_conds
-"""
 
 # %%
 def show_all(obj):
@@ -174,63 +179,84 @@ def preprocess(df):
     return df
 
 # %%
-def make_dict_containing_all_info(num_subjects: int,
-                                  folder_path: str = "04_RawData") -> pd.DataFrame:
+def make_empty_hierarchical_df(num_subjects, exp_conditions, num_agents):
+    df_empty = {}
+    
+    for subjectID in range(1, num_subjects+1):
+        df_empty[f"ID{subjectID}"] = {}
+        
+    for subjectID in range(1, num_subjects+1):
+        for condition in exp_conditions:
+            df_empty[f"ID{subjectID}"][condition] = {}
+    
+    for subjectID in range(1, num_subjects+1):
+        for condition in exp_conditions:
+            for agent in num_agents:
+                df_empty[f"ID{subjectID}"][condition][f"agents{agent}"] = {}
+
+    return df_empty
+
+# %%
+def make_dict_of_all_info(num_subjects: int,
+                          folder_path: str = "04_RawData") -> pd.DataFrame:
     """
     Make a dictionary that contains all subjects' records.
     The dictionary's hierarchy is: 
-        participant's ID:
-            experiment conditions (control, urgent, nonurgent, omoiyari):
-                trial (trial number and number of agents)
+        1. participant's ID
+        2. experiment conditions (urgent, nonurgent, omoiyari)
+        3. number of agents (5, 10, 20)
+        4. trial (1-8)
                 
-    ex. df_1_omoiyari_5_1 = df_all_subjects["ID1"]["omoiyari"]["agents5_tri1"]
+    ex. df = df_all["ID1"]["omoiyari"]["agents5"]["trial1"]
     """
     pd.options.mode.chained_assignment = None # otherwise there'll be many warnings
     
     exp_conditions = ["urgent", "nonurgent", "omoiyari"] #, "control"]
+    num_agents = [5, 10, 20]
     
-    df_all_subjects = {}
-    for subjectID in range(1, num_subjects+1):
-        df_all_subjects[f"ID{subjectID}"] = {}
-    
-    with tqdm(total=len(exp_conditions)*num_subjects) as pbar:
-        for condition in exp_conditions:
-            for ID in range(1, 30):
+    df_all = make_empty_hierarchical_df(num_subjects, exp_conditions, num_agents)
+        
+    for ID in tqdm(range(1, num_subjects+1)):
+        for cond in exp_conditions:
+        
+            df = pd.read_csv(f'{folder_path}/{ID}_{cond}.csv')
             
-                df = pd.read_csv(f'{folder_path}/{ID}_{condition}.csv')
-                
-                df_5 = df.query('type.str.contains("05")')
-                df_10 = df.query('type.str.contains("10")')
-                df_20 = df.query('type.str.contains("20")')
-                
-                trialnum5 = list(set(df_5['trial']))
-                trialnum10 = list(set(df_10['trial']))
-                trialnum20 = list(set(df_20['trial']))
-                assert len(trialnum5) == len(trialnum10) == len(trialnum20), "all trials must have same lengths"
-                
-                dfs_per_trials = {}
+            df_5 = df.query('type.str.contains("05")')
+            df_10 = df.query('type.str.contains("10")')
+            df_20 = df.query('type.str.contains("20")')
+            
+            dfs = [df_5, df_10, df_20]
+            
+            trialnum5 = list(set(df_5['trial']))
+            trialnum10 = list(set(df_10['trial']))
+            trialnum20 = list(set(df_20['trial']))
+            
+            trialnums = [trialnum5, trialnum10, trialnum20]
+            
+            for df_agent, trialnum, agent in zip(dfs, trialnums, num_agents):
                 for trial in range(len(trialnum5)):
                     
-                    df_5_tri = df_5.query("trial == @trialnum5[@trial]")
-                    df_5_tri = preprocess(df_5_tri)
-                    
-                    df_10_tri = df_10.query("trial == @trialnum10[@trial]")
-                    df_10_tri = preprocess(df_10_tri)
-                    
-                    df_20_tri = df_20.query("trial == @trialnum20[@trial]")
-                    df_20_tri = preprocess(df_20_tri)
-                    
-                    dfs_per_trials[f"agents5_tri{trial+1}"] = df_5_tri
-                    dfs_per_trials[f"agents10_tri{trial+1}"] = df_10_tri
-                    dfs_per_trials[f"agents20_tri{trial+1}"] = df_20_tri
-                                    
-                df_all_subjects[f"ID{ID}"][f"{condition}"] = dfs_per_trials
-                pbar.update(1)
+                    df_tri = df_agent.query('trial == @trialnum[@trial]')
+                    df_tri = preprocess(df_tri)
 
-    return df_all_subjects
+                    df_all[f"ID{ID}"][cond][f"agents{agent}"][f"trial{trial+1}"] = df_tri
+
+                        
+    return df_all
 
 # %%
-def plot_traj_per_trials(df_all_subjects: pd.DataFrame, 
+def make_df_trial(df_all: pd.DataFrame, 
+                  ID: int, 
+                  condition: Literal['urgent', 'nonurgent', 'omoiyari'], 
+                  num_agents: int, 
+                  trial: int) -> pd.DataFrame:
+    
+    df = df_all[f'ID{ID}'][condition][f"agents{num_agents}"][f"trial{trial}"]
+    
+    return df
+    
+# %%
+def plot_traj_per_trials(df_all: pd.DataFrame, 
                          ID: int, 
                          conditions: Literal["urgent", "nonurgent", "omoiyari"], 
                          num_agents: Literal[5, 10, 20]) -> None:
@@ -239,16 +265,16 @@ def plot_traj_per_trials(df_all_subjects: pd.DataFrame,
     """
     fig = plt.figure(figsize=(12, 6))
     for trial, color in zip(range(1, 9), mcolors.TABLEAU_COLORS):
-        df = df_all_subjects[f"ID{ID}"][conditions][f"agents{num_agents}_tri{trial}"]
+        df = df_all[f"ID{ID}"][conditions][f"agents{num_agents}"][f"trial{trial}"]
         ax = fig.add_subplot(2, 4, trial)
         for x, y in zip(df['posX'], df['posY']):
             ax.scatter(x, y, color=color, alpha=.5)
-        ax.set_title(f"tri{trial}")
+        ax.set_title(f"trial{trial}")
     plt.suptitle(f"ID{ID}_{conditions}_agents{num_agents}")
     plt.show()
     
 # %%
-def plot_traj_compare_conds(df_all_subjects: pd.DataFrame, 
+def plot_traj_compare_conds(df_all: pd.DataFrame, 
                             ID: int, 
                             num_agents: Literal[5, 10, 20]) -> None:
     """
@@ -259,14 +285,14 @@ def plot_traj_compare_conds(df_all_subjects: pd.DataFrame,
     conditions = ["urgent", "nonurgent", "omoiyari"]
     num_all_trials = 8
     with tqdm(total=len(conditions)*num_all_trials) as pbar:
-        for i, condition in enumerate(conditions):
+        for i, cond in enumerate(conditions):
             ax = fig.add_subplot(1, 3, i+1)
             for trial, color in zip(range(1, num_all_trials+1), mcolors.TABLEAU_COLORS):
-                df = df_all_subjects[f"ID{ID}"][condition][f"agents{num_agents}_tri{trial}"]
+                df = df_all[f"ID{ID}"][cond][f"agents{num_agents}"][f"trial{trial}"]
                 for x, y in zip(df['posX'], df['posY']):
                     ax.scatter(x, y, color=color, alpha=.5)
                 pbar.update(1)
-            ax.set_title(f"ID{ID}_{condition}_agents{num_agents}")
+            ax.set_title(f"ID{ID}_{cond}_agents{num_agents}")
     print("plotting...")
     plt.show()
 
--- re-analysis.py
+++ re-analysis.py
@@ -1,4 +1,3 @@
-# control, urgent, nonurgent, omoiyari
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,10 +6,9 @@ import seaborn as sns
 import myfuncs as mf
 from gc import collect as g
 
-df_all = mf.make_dict_containing_all_info(29)
+df_all = mf.make_dict_of_all_info(29)
 
 mf.plot_traj_per_trials(df_all, 1, 'urgent', 5)
 mf.plot_traj_compare_conds(df_all, 1, 5)
 
-df_small = df_all["ID3"]["nonurgent"]
-df_part = df_small["agents10_tri2"]
+df_part = mf.make_df_trial(df_all, 3, 'nonurgent', 10, 2)
