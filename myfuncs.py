2fff930f18fc47632aef79c87eb7b35d100ede1e
Tomoki-Osaki <hantoumeiningenn@gmail.com>
2024-10-02 15:00:56 +0900

    applied the processes to all data

--- myfuncs.py
+++ myfuncs.py
@@ -77,6 +77,56 @@ def _add_cols_ideal_positions(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 # %%
+def drop_unnecessary_cols(df):
+    df.dropna(axis=1, inplace=True)
+    cols_to_drop = []
+    for i in range(6, 12, 5):
+        if all(x == 0 for x in df[f"other{i}NextX"]):
+            for j in range(i, 21):
+                other_cols = [f"other{j}NextX", f"other{j}NextY", 
+                              f"other{j}MoveX", f"other{j}MoveY",
+                              f"other{j}Coll"]    
+                cols_to_drop.extend(other_cols)
+            
+    df.drop(cols_to_drop, axis=1, inplace=True)        
+    
+    return df
+
+# %%
+def _calc_distance(myX, myY, anotherX, anotherY):
+    mypos = np.array([myX, myY])
+    anotherpos = np.array([anotherX, anotherY])
+    distance = np.linalg.norm(mypos - anotherpos)
+    
+    return distance
+
+# %%
+def add_cols_dist_others(df):
+    df_tmp = pd.DataFrame()
+    for i in range(1, 21):
+        try:
+            dist_others = df.apply(lambda df_: _calc_distance(
+               df_["myNextX"], df_["myNextY"], df_[f"other{i}NextX"], df_[f"other{i}NextY"]
+               ), axis=1)
+            df_tmp[f"distOther{i}"] = dist_others
+        except KeyError:
+            break
+    
+    newdf = pd.concat([df, df_tmp], axis=1)
+    
+    return newdf
+
+# %%
+def preprocess(df):
+    df.reset_index(drop=True, inplace=True)
+    df = _make_start_from_UL(df)
+    df = _add_cols_ideal_positions(df)
+    df = drop_unnecessary_cols(df)
+    df = add_cols_dist_others(df)
+    
+    return df
+
+# %%
 def make_dict_containing_all_info(num_subjects: int,
                                   folder_path: str = "04_RawData") -> pd.DataFrame:
     """
@@ -115,19 +165,13 @@ def make_dict_containing_all_info(num_subjects: int,
                 for trial in range(len(trialnum5)):
                     
                     df_5_tri = df_5.query("trial == @trialnum5[@trial]")
-                    df_5_tri.reset_index(drop=True, inplace=True)
-                    df_5_tri = _make_start_from_UL(df_5_tri)
-                    df_5_tri = _add_cols_ideal_positions(df_5_tri)
+                    df_5_tri = preprocess(df_5_tri)
                     
                     df_10_tri = df_10.query("trial == @trialnum10[@trial]")
-                    df_10_tri.reset_index(drop=True, inplace=True)
-                    df_10_tri = _make_start_from_UL(df_10_tri)
-                    df_10_tri = _add_cols_ideal_positions(df_10_tri)
+                    df_10_tri = preprocess(df_10_tri)
                     
                     df_20_tri = df_20.query("trial == @trialnum20[@trial]")
-                    df_20_tri.reset_index(drop=True, inplace=True)
-                    df_20_tri = _make_start_from_UL(df_20_tri)
-                    df_20_tri = _add_cols_ideal_positions(df_20_tri)
+                    df_20_tri = preprocess(df_20_tri)
                     
                     dfs_per_trials[f"agents5_tri{trial+1}"] = df_5_tri
                     dfs_per_trials[f"agents10_tri{trial+1}"] = df_10_tri
@@ -196,4 +240,4 @@ def plot_traj_compare_conds(df_all_subjects: pd.DataFrame,
     print("plotting...")
     plt.show()
 
-# %%
+# %% 
--- re-analysis.py
+++ re-analysis.py
@@ -10,30 +10,14 @@ from gc import collect as gc
 df_all = mf.make_dict_containing_all_info(29)
 
 df_small = df_all["ID3"]["omoiyari"]
-df_part = df_small["agents5_tri1"]
+df_part = df_small["agents10_tri1"]
+
 
 dists = []
 for i in range(len(df_part.index)):
     other = np.array([df_part.other1NextX[i], df_part.other1NextY[i]])
     own = np.array([df_part.myNextX[i], df_part.myNextY[i]])
     dists.append(np.linalg.norm(other - own))
-
-def calc_distance(myX, myY, otherX, otherY):
-    mypos = np.array([myX, myY])
-    otherpos = np.array([otherX, otherY])
-    distance = np.linalg.norm(mypos - otherpos)
-    
-    return distance
-    
-dic = pd.DataFrame(columns=([f"other{i}" for i in range(1, 21)]))
-for i in range(1, 21):
-     tmp = df_part.apply(lambda df: calc_distance(
-        df["myNextX"], df["myNextY"], df[f"other{i}NextX"], df[f"other{i}NextY"]
-        ), axis=1)
-     dic[f"other{i}"] = tmp
-
-newdf = pd.concat([df_part, dic], axis=1)
-newdf.dropna(axis=1, inplace=True)
 
 
 # mf.plot_traj_per_trials(df_all, 1, 'urgent', 5)
