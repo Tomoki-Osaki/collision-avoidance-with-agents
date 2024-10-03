a8be3e0a178d68b0b9f05ee1be041c07694de0b1
Tomoki Osaki <hantoumeiningenn@gmail.com>
2024-10-03 11:16:59 +0900

    plot dists

--- re-analysis.py
+++ re-analysis.py
@@ -6,11 +6,29 @@ import seaborn as sns
 import myfuncs as mf
 from gc import collect as g
 
+SUBJECTS = mf.SUBJECTS
+CONDITIONS = mf.CONDITIONS # [urgent, nonurgent, omoiyari]
+AGENTS = mf.AGENTS
+NUM_AGENTS = mf.NUM_AGENTS
+TRIALS = mf.TRIALS
+
 df_all = mf.make_dict_of_all_info()
 
 mf.plot_traj_per_trials(df_all, 1, 'urgent', 5)
 mf.plot_traj_compare_conds(df_all, 1, 5)
 
 df_part = mf.make_df_trial(df_all, 3, 'nonurgent', 10, 2)
+cols = df_part.columns
 
+for cond in CONDITIONS:
+    df_small = df_all['ID1'][cond]['agents20']
+    for tri in TRIALS:
+        plt.plot(df_small[f'trial{tri}']['closest_dists'])
+    plt.show()
+
+for cond in CONDITIONS:
+    df_small = df_all['ID1'][cond]['agents20']
+    for tri in TRIALS:
+        plt.plot(df_small[f'trial{tri}']['dist_real_ideal'])
+    plt.show()
 
