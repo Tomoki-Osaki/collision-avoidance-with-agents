7b4d13a99e9761ac9d1f9a28a709e475c2b2e714
Tomoki Osaki <hantoumeiningenn@gmail.com>
2024-10-06 01:16:41 +0900

    calculate the degree

--- re-analysis.py
+++ re-analysis.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import seaborn as sns
-import myfuncs as mf
 from gc import collect as g
 from typing import Literal
+import math
+import myfuncs as mf
 
 SUBJECTS = mf.SUBJECTS
 CONDITIONS = mf.CONDITIONS # [urgent, nonurgent, omoiyari]
@@ -18,13 +19,11 @@ df_all = mf.make_dict_of_all_info(SUBJECTS)
 ID = 11
 agents = 20
 cond = "nonurgent"
-trial = 2
+trial = 4
 
+# %% plot functions
 mf.plot_traj_per_trials(df_all, ID, cond, agents)
 mf.plot_traj_compare_conds(df_all, ID, agents)
-
-df_trial = mf.make_df_trial(df_all, ID, cond, agents, trial)
-cols = df_trial.columns
 
 mf.plot_dist_compare_conds(df_all, ID, agents, "dist_actual_ideal")
 mf.plot_dist_per_cond(df_all, ID, agents, "dist_actual_ideal")
@@ -32,8 +31,39 @@ mf.plot_dist_per_cond(df_all, ID, agents, "dist_actual_ideal")
 mf.plot_dist_compare_conds(df_all, ID, agents, "closest_dists")
 mf.plot_dist_per_cond(df_all, ID, agents, "closest_dists")
 
+# %%
+df_trial = mf.make_df_trial(df_all, ID, cond, agents, trial)
+cols = df_trial.columns
 
+posXtplus1  = pd.concat([df_trial.posX[1:], pd.Series([None])])
+posYtplus1  = pd.concat([df_trial.posY[1:], pd.Series([None])])
+df_trial['posXt+1'] = posXtplus1
+df_trial['posYt+1'] = posYtplus1
 
+def _calc_distance(myX, myY, anotherX, anotherY):
+    mypos = np.array([myX, myY])
+    anotherpos = np.array([anotherX, anotherY])
+    distance = np.linalg.norm(mypos - anotherpos)
+    
+    return distance
+
+def calc_deg(x0, y0, x1, y1, x2, y2):
+    #角度計算開始
+    try:
+        vec1 = [x1 - x0, y1 - y0]
+        vec2 = [x2 - x0, y2 - y0]
+    except TypeError:
+        return None
+    
+    absvec1 = np.linalg.norm(vec1)
+    absvec2 = np.linalg.norm(vec2)
+    inner = np.inner(vec1, vec2)
+    cos_theta = inner / (absvec1 * absvec2)
+    theta = math.degrees(math.acos(cos_theta))
+    
+    return theta
+
+# %% time series clustering
 df_clustering = mf.make_df_for_clustering(df_all, 8, 20, "dist_actual_ideal")
 true_labels = ["omoiyari", "isogi", "yukkuri"] * 8
 
@@ -98,6 +128,4 @@ find_proper_num_clusters(df_clustering)
 # df_a.dropna(inplace=True, axis=1)
 # df_a = df_a.query("trial != 0")
 # df_a["agents"] = None
-
-
 
