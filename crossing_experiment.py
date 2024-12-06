bcf79db46b62fe3ca3a00c5937ed21de4c5076a4
Tomoki-Osaki <hantoumeiningenn@gmail.com>
2024-12-06 12:57:47 +0900

    made a func to create animations

--- crossing_experiment.py
+++ crossing_experiment.py
@@ -2,24 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import glob
 
-df = pd.read_csv("C:/Users/ootmo/OneDrive/Desktop/修論/トヨタ_共同プロジェクト/collision-avoidance-with-agents/glob_shaped/20220405_all_group_ha_keiro1_index_1_8_11_to_0_2.bag.csv")
-for col in df.columns: print(col)
+path = 'crossing_exp/glob_shaped/*.csv'
+flist = glob.glob(path)
 
-plt.plot(df['/vrpn_client_node/body_0/pose/field.pose.position.x'],
-         df['/vrpn_client_node/body_0/pose/field.pose.position.z'])
-plt.plot(df['/vrpn_client_node/body_1/pose/field.pose.position.x'],
-         df['/vrpn_client_node/body_1/pose/field.pose.position.z'])
+df = pd.read_csv(flist[15])
+df['ped0_body_posx'] = df['/vrpn_client_node/body_0/pose/field.pose.position.x']
+df['ped0_body_posy'] = df['/vrpn_client_node/body_0/pose/field.pose.position.z']
+df['ped1_body_posx'] = df['/vrpn_client_node/body_1/pose/field.pose.position.x']
+df['ped1_body_posy'] = df['/vrpn_client_node/body_1/pose/field.pose.position.z']
 
-for x1, y1, x2, y2 in zip(
-        df['/vrpn_client_node/body_0/pose/field.pose.position.x'],
-        df['/vrpn_client_node/body_0/pose/field.pose.position.z'],
-        df['/vrpn_client_node/body_1/pose/field.pose.position.x'],
-        df['/vrpn_client_node/body_1/pose/field.pose.position.z']
-        ):
-    plt.scatter(x1, y1, color='red')
-    plt.scatter(x2, y2, color='blue')
-plt.show()
+# for col in df.columns: print(col)
+
+def plot_traj(df):
+    for x1, y1, x2, y2 in zip(df['ped0_body_posx'], df['ped0_body_posy'], 
+                              df['ped1_body_posx'], df['ped1_body_posy']):
+        plt.scatter(x1, y1, color='red')
+        plt.scatter(x2, y2, color='blue')
+    plt.show()
 
 
 tcpa = 0 # Time to Closest of Point of Approach
@@ -31,22 +32,53 @@ def calc_braking_rate(a1=-5.145, b1=3.348, c1=4.286, d1=-13.689):
                     
     return braking_index
 
-pos_data = zip(df['/vrpn_client_node/body_0/pose/field.pose.position.x'],
-               df['/vrpn_client_node/body_0/pose/field.pose.position.z'],
-               df['/vrpn_client_node/body_1/pose/field.pose.position.x'],
-               df['/vrpn_client_node/body_1/pose/field.pose.position.z'])
+
+xmax = max([max(df['ped0_body_posx']), max(df['ped1_body_posx'])])
+xmin = min([min(df['ped0_body_posx']), min(df['ped1_body_posx'])])
+ymax = max([max(df['ped0_body_posy']), max(df['ped1_body_posy'])])
+ymin = min([min(df['ped0_body_posy']), min(df['ped1_body_posy'])])
+
+frames= zip(df.index,
+            df['ped0_body_posx'], df['ped0_body_posy'], 
+            df['ped1_body_posx'], df['ped1_body_posy'])
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set_xlim(-4, 4)
-ax.set_ylim(-4, 4)
+alpha = 0.5
+delay = 1
+keep_former_step = False
 
+x1, y1, x2, y2 = [], [], [], []
 def update(frame):
-    ax.scatter(frame[0], frame[1], color='red')
-    ax.scatter(frame[2], frame[3], color='blue')
+    ax.cla()
+    ax.set_xlim(xmin-1, xmax+1)
+    ax.set_ylim(ymin-1, ymax+1)
+    ax.grid()
     
-anim = FuncAnimation(fig, update, frames=pos_data, interval=200)
+    x1.append(frame[1])
+    y1.append(frame[2])
+    x2.append(frame[3])
+    y2.append(frame[4])
+    
+    # if you want to keep the former steps
+    if keep_former_step == True:
+        ax.scatter(x1, y1, color='red', alpha=0.5)
+        ax.scatter(x2, y2, color='blue', alpha=0.5)
+    
+    else:
+        if frame[0] <= delay:
+            ax.scatter(x1, y1, color='red', alpha=0.5)
+            ax.scatter(x2, y2, color='blue', alpha=0.5)
+        else:
+            ax.scatter(x1[frame[0]-delay:], y1[frame[0]-delay:], color='red', alpha=alpha)
+            ax.scatter(x2[frame[0]-delay:], y2[frame[0]-delay:], color='blue', alpha=alpha)
 
-anim.save("c03.gif", writer='pillow')
-plt.close()
+    ax.text(xmax-1.2, ymax+1.2, f'time (100ms) : {frame[0]}')
+    
+anim = FuncAnimation(fig, update, frames=frames, interval=200, 
+                     repeat=False, cache_frame_data=False)
 
+plt.show()
+
+# anim.save("crossing.mp4", writer='ffmpeg')
+# plt.close()
