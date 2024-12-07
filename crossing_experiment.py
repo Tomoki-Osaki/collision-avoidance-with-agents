c7138b2e916e65225e390f89923a97b39716e188
Tomoki-Osaki <hantoumeiningenn@gmail.com>
2024-12-07 20:51:14 +0900

    made animation more informational

--- crossing_experiment.py
+++ crossing_experiment.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import glob
+import funcs_calc_index as ci
 
 path = 'crossing_exp/glob_shaped/*.csv'
 flist = glob.glob(path)
@@ -18,20 +19,111 @@ path = "crossing_exp/glob_shaped/20220405_all_group_ha_keiro10_index_10_14_1_to_
 
 #df = pd.read_csv(flist[3])
 df = pd.read_csv(path)
-df['ped0_body_posx'] = df['/vrpn_client_node/body_0/pose/field.pose.position.x']
-df['ped0_body_posy'] = df['/vrpn_client_node/body_0/pose/field.pose.position.z']
-df['ped1_body_posx'] = df['/vrpn_client_node/body_1/pose/field.pose.position.x']
-df['ped1_body_posy'] = df['/vrpn_client_node/body_1/pose/field.pose.position.z']
+df = pd.read_csv(flist[0])
+path = "crossing_exp/glob_shaped/20220405_all_group_ha_keiro10_index_10_14_1_to_8_9.bag.csv"
+df = pd.read_csv(path)
+
+df = df[['/vrpn_client_node/body_0/pose/field.pose.position.x',
+         '/vrpn_client_node/body_0/pose/field.pose.position.z',
+         'body_0_vel_x', 
+         'body_0_vel_z',
+         '/vrpn_client_node/body_1/pose/field.pose.position.x',
+         '/vrpn_client_node/body_1/pose/field.pose.position.z',
+         'body_1_vel_x', 
+         'body_1_vel_z']]
+
+old_new_cols = {'/vrpn_client_node/body_0/pose/field.pose.position.x': 'B_posx1',
+                '/vrpn_client_node/body_0/pose/field.pose.position.z': 'C_posy1',
+                'body_0_vel_x': 'D_velx1',
+                'body_0_vel_z': 'E_vely1',
+                '/vrpn_client_node/body_1/pose/field.pose.position.x': 'F_posx2',
+                '/vrpn_client_node/body_1/pose/field.pose.position.z': 'G_posy2',
+                'body_1_vel_x': 'H_velx2',
+                'body_1_vel_z': 'I_vely2'}
+
+df = df.rename(columns=old_new_cols)
+
+df['J_CPx'] = ci.J_CPx(df)
+df['K_CPy'] = ci.K_CPy(df)
+df['L_TTCP0'] = df.apply(ci.L_TTCP0, axis=1)
+df['M_TTCP1'] = df.apply(ci.M_TTCP1, axis=1)
+df['N_deltaTTCP'] = ci.N_deltaTTCP(df)
+df['O_Judge'] = ci.O_Judge(df)
+df['P_JudgeEntropy'] = ci.P_JudgeEntropy(df) # does not match!
+df['Q_equA'] = ci.Q_equA(df)
+df['R_equB'] = ci.R_equB(df)
+df['S_equC'] = ci.S_equC(df)
+df['T_TCPA'] = ci.T_TCPA(df) 
+df['U_DCPA'] = ci.U_DCPA(df)
+df['V_BrakingRate'] = df.apply(ci.V_BrakingRate, axis=1)
+df['W_distance'] = df.apply(ci.W_distance, axis=1)
 
 # for col in df.columns: print(col)
 
 def plot_traj(df):
-    for x1, y1, x2, y2 in zip(df['ped0_body_posx'], df['ped0_body_posy'], 
-                              df['ped1_body_posx'], df['ped1_body_posy']):
+    for x1, y1, x2, y2 in zip(df['B_posx1'], df['C_posy1'], 
+                              df['F_posx2'], df['G_posy2']):
         plt.scatter(x1, y1, color='red')
         plt.scatter(x2, y2, color='blue')
     plt.show()
 
+# %%
+alpha = 0.5
+fig, ax = plt.subplots(1, 3, figsize=(16, 6))
+
+rep = 0
+headwidth = 8
+headlength = 8
+linewidth = 3
+BrakingRate = []
+distance = []
+dist_ymax = max(df['W_distance'])
+xmax = len(df)
+def update(frame):
+    global rep
+    BrakingRate.append(frame[1]['V_BrakingRate'])
+    distance.append(frame[1]['W_distance'])
+    rep += 1
+    ax[0].cla()
+    ax[0].set_title('Moving of pedestrians')
+    ax[0].set_xlim(-5, 5)
+    ax[0].set_ylim(-5, 5)
+    ax[0].grid()
+    ax[0].scatter(frame[1]['B_posx1'], frame[1]['C_posy1'], s=100, c='blue', alpha=alpha)
+    ax[0].scatter(frame[1]['F_posx2'], frame[1]['G_posy2'], s=100, c='red', alpha=alpha)
+    if not rep == len(df):
+        ax[0].annotate('', xy=(df['B_posx1'][frame[0]+1], df['C_posy1'][frame[0]+1]), 
+                       xytext=(frame[1]['B_posx1'], frame[1]['C_posy1']),
+                       arrowprops=dict(shrink=0, width=1, headwidth=headwidth, 
+                                       headlength=headlength,
+                                       facecolor='gray', edgecolor='blue'))
+        
+        ax[0].annotate('', xy=(df['F_posx2'][frame[0]+1], df['G_posy2'][frame[0]+1]), 
+                       xytext=(frame[1]['F_posx2'], frame[1]['G_posy2']),
+                       arrowprops=dict(shrink=0, width=1, headwidth=headwidth, 
+                                       headlength=headlength,
+                                       facecolor='gray', edgecolor='red'))
+    ax[1].cla()
+    ax[1].set_title('Braking rate')
+    ax[1].set_xlim(0, xmax+1)
+    ax[1].set_ylim(-0.01, 1)
+    ax[1].grid()
+    ax[1].plot(BrakingRate, lw=linewidth, color='green')
+    
+    ax[2].cla()
+    ax[2].set_title('Distance between pedestrians')
+    ax[2].set_xlim(0, xmax+1)
+    ax[2].set_ylim(0, dist_ymax+0.5)
+    ax[2].grid()
+    ax[2].plot(distance, lw=linewidth, color='orange')
+
+anim = FuncAnimation(fig, update, frames=df.iterrows(), repeat=False, 
+                     interval=200, cache_frame_data=False)
+plt.show()
+# anim.save("crossing.mp4", writer='ffmpeg')
+# plt.close()
+
+# %%
 xmax = max([max(df['ped0_body_posx']), max(df['ped1_body_posx'])])
 xmin = min([min(df['ped0_body_posx']), min(df['ped1_body_posx'])])
 ymax = max([max(df['ped0_body_posy']), max(df['ped1_body_posy'])])
@@ -43,7 +135,6 @@ frames= zip(df.index,
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-alpha = 0.5
 delay = 0
 keep_former_step = False
 
@@ -81,212 +172,4 @@ anim = FuncAnimation(fig, update, frames=frames, interval=200,
 
 anim.save("crossing.mp4", writer='ffmpeg')
 plt.close()
-
-# %% calculate the braking rate
-df = pd.read_csv(flist[0])
-path = "crossing_exp/glob_shaped/20220405_all_group_ha_keiro10_index_10_14_1_to_8_9.bag.csv"
-df = pd.read_csv(path)
-df = df[['/vrpn_client_node/body_0/pose/field.pose.position.x',
-         '/vrpn_client_node/body_0/pose/field.pose.position.z',
-         'body_0_vel_x', 
-         'body_0_vel_z',
-         '/vrpn_client_node/body_1/pose/field.pose.position.x',
-         '/vrpn_client_node/body_1/pose/field.pose.position.z',
-         'body_1_vel_x', 
-         'body_1_vel_z']]
-
-old_new_cols = {'/vrpn_client_node/body_0/pose/field.pose.position.x': 'B_posx1',
-                '/vrpn_client_node/body_0/pose/field.pose.position.z': 'C_posy1',
-                'body_0_vel_x': 'D_velx1',
-                'body_0_vel_z': 'E_vely1',
-                '/vrpn_client_node/body_1/pose/field.pose.position.x': 'F_posx2',
-                '/vrpn_client_node/body_1/pose/field.pose.position.z': 'G_posy2',
-                'body_1_vel_x': 'H_velx2',
-                'body_1_vel_z': 'I_vely2'}
-
-df = df.rename(columns=old_new_cols)
-
-"""
-B_posx1, C_posy1, D_velx1, E_vely1, F_posx2, 
-G_posy2, H_velx2, I_vely2, J_CPx, K_CPy
-"""
-
-def J_CPx(df):
-    """
-    =IFERROR( 
-        (H2*$E2*$B2 - $D2*I2*F2 + $D2*H2*(G2-$C2)) / (H2*$E2 - $D2*I2)
-    , "")
-    """
-    nume = df['H_velx2']*df['E_vely1']*df['B_posx1'] - \
-           df['D_velx1']*df['I_vely2']*df['F_posx2'] + \
-           df['D_velx1']*df['H_velx2']*(df['G_posy2'] - df['C_posy1'])
-           
-    deno = df['H_velx2']*df['E_vely1'] - df['D_velx1']*df['I_vely2']
-    val = nume / deno    
-    return val
-
-def K_CPy(df):
-    """
-    =IFERROR( 
-        (E2/D2) * (J2 - B2) + C2
-    , "")
-    """
-    val = (df['E_vely1'] / df['D_velx1']) * (df['J_CPx'] - df['B_posx1']) + df['C_posy1']
-    return val
-
-def L_TTCP0(df):
-    """
-    =IF(
-        AND (
-            OR ( AND (B2 < J2, D2 > 0), 
-                 AND (B2 > J2, D2 < 0)), 
-            OR ( AND (C2 < K2, E2 > 0), 
-                 AND (C2 > K2, E2 < 0))
-            ), 
-        SQRT(( J2 - $B2 )^2 + (K2 - $C2)^2) / ( SQRT(($D2^2 + $E2^2 )))
-    , "")
-    """
-    if ( ((df['B_posx1'] < df['J_CPx'] and df['D_velx1'] > 0) or
-          (df['B_posx1'] > df['J_CPx'] and df['D_velx1'] < 0)) 
-        and
-         ((df['C_posy1'] < df['K_CPy'] and df['E_vely1'] > 0) or
-          (df['C_posy1'] > df['K_CPy'] and df['E_vely1'] < 0))
-        ):
-        
-        nume = np.sqrt((df['J_CPx'] - df['B_posx1'])**2 + (df['K_CPy'] - df['C_posy1'])**2)
-        deno = np.sqrt((df['D_velx1']**2 + df['E_vely1']**2))
-        val = nume / deno
-        return val 
-    
-    else:
-        return None
-
-def M_TTCP1(df):
-    """
-    =IF( 
-        AND (
-            OR ( AND (F2 < J2, H2 > 0), 
-                 AND (F2 > J2, H2 < 0)), 
-            OR ( AND (G2 < K2, I2 > 0), 
-                 AND (G2 > K2, I2 < 0))
-            ), 
-        SQRT( (J2 - F2)^2 + (K2 - G2)^2 ) / (SQRT( (H2^2 + I2^2) ))
-    , "")
-
-    """
-    if ( ((df['F_posx2'] < df['J_CPx'] and df['H_velx2'] > 0) or
-          (df['F_posx2'] > df['J_CPx'] and df['H_velx2'] < 0)) 
-        and
-         ((df['G_posy2'] < df['K_CPy'] and df['I_vely2'] > 0) or
-          (df['G_posy2'] > df['K_CPy'] and df['I_vely2'] < 0))
-        ):
-        
-        nume = np.sqrt((df['J_CPx'] - df['F_posx2'])**2 + (df['K_CPy'] - df['G_posy2'])**2)
-        deno = np.sqrt((df['H_velx2']**2 + df['I_vely2']**2))
-        val = nume / deno
-        return val
-    
-    else:
-        return None 
-
-def N_deltaTTCP(df):
-    """
-    =IFERROR( 
-        ABS(L2 - M2)
-    , -1)
-    """
-    val = abs(df['L_TTCP0'] - df['M_TTCP1'])
-    return val
-
-def O_Judge(df, eta1=-0.303, eta2=0.61):
-    """
-    =IFERROR(
-        1 / (1 + EXP($DB$1[eta1] + $DC$1[eta2]*(M2 - L2)))
-    , "")
-    """
-    val = 1 / (1 + np.exp(eta1 + eta2*(df['M_TTCP1'] - df['L_TTCP0'])))
-    return val
-
-def P_JudgeEntropy(df): # does not match!
-    """
-    =IFERROR( 
-        -O2 * LOG(O2) - (1 - O2) * LOG(1 - O2)
-    , "")
-    """
-    val = -df['O_Judge'] * np.log(df['O_Judge']) - (1 - df['O_Judge']) * np.log(1 - df['O_Judge'])
-    return val
-
-def Q_equA(df):
-    """
-    = ($D2 - H2)^2 + ($E2 - I2)^2
-    """
-    val = (df['D_velx1'] - df['H_velx2'])**2 + (df['E_vely1'] - df['I_vely2'])**2
-    return val
-    
-def R_equB(df):
-    """
-    = (2*($D2 - H2)*($B2 - F2)) + (2*($E2 - I2)*($C2 - G2))
-    """
-    val = (2 * (df['D_velx1'] - df['H_velx2']) * (df['B_posx1'] - df['F_posx2'])) + \
-          (2 * (df['E_vely1'] - df['I_vely2']) * (df['C_posy1'] - df['G_posy2']))
-    return val
-
-def S_equC(df):
-    """
-    = ($B2 - F2)^2 + ($C2 - G2)^2
-    """
-    val = (df['B_posx1'] - df['F_posx2'])**2 + (df['C_posy1'] - df['G_posy2'])**2
-    return val
-
-def T_TCPA(df): 
-    """
-    = -(R2 / (2*Q2))
-    """
-    val = -(df['R_equB'] / (2*df['Q_equA']))
-    return val
-
-def U_DCPA(df):
-    """
-    = SQRT( (-(R2^2) + (4*Q2*S2)) / (4*Q2) ) 
-    """
-    val = np.sqrt(((-df['R_equB']**2) + (4*df['Q_equA']*df['S_equC'])) / (4*df['Q_equA']))
-    return val
-
-def V_BrakingRate(df, a1=-0.034, b1=3.348, c1=4.252, d1=-0.003):
-    """
-    a1: -5.145 (-0.034298)
-    b1: 3.348 (3.348394)
-    c1: 4.286 (4.252840)
-    d1: -13.689 (-0.003423)
-    
-    =IF(T2 < 0, "", 
-    IFERROR(
-        (1 / (1 + EXP(-($DD$1[c1] + ($DE$1[d1]*T2*1000))))) * 
-        (1 / (1 + EXP(-($DF$1[b1] + ($DG$1[a1]*30*U2)))))
-        , "")
-    )
-    """
-    if df['T_TCPA'] < 0:
-        return None
-    else:
-        term1 = (1 / (1 + np.exp(-(c1 + (d1*df['T_TCPA']*1000)))))
-        term2 = (1 / (1 + np.exp(-(b1 + (a1*df['U_DCPA']*30)))))
-        val = term1 * term2
-        return val
-        
-df['J_CPx'] = J_CPx(df)
-df['K_CPy'] = K_CPy(df)
-df['L_TTCP0'] = df.apply(L_TTCP0, axis=1)
-df['M_TTCP1'] = df.apply(M_TTCP1, axis=1)
-df['N_deltaTTCP'] = N_deltaTTCP(df)
-df['O_Judge'] = O_Judge(df)
-df['P_JudgeEntropy'] = P_JudgeEntropy(df) # does not match!
-df['Q_equA'] = Q_equA(df)
-df['R_equB'] = R_equB(df)
-df['S_equC'] = S_equC(df)
-df['T_TCPA'] = T_TCPA(df) 
-df['U_DCPA'] = U_DCPA(df)
-df['V_BrakingRate'] = df.apply(V_BrakingRate(df))
-
-df.to_csv('res.csv')
 
