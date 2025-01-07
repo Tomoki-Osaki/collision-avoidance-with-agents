f4298820751aec5e725b5c96a6ad6c5861835911
Tomoki Osaki <hantoumeiningenn@gmail.com>
2025-01-07 10:35:26 +0900

    separate the files by each role

---
+++ doSimulation.py
@@ -0,0 +1,250 @@
+""" 
+シミュレーションを実際に実行する
+2025/01/07 
+"""
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import tqdm, gc
+import classSimulation
+import funcSimulation
+
+# %%
+INTERVAL = funcSimulation.INTERVAL # 100
+TRIAL = 1 # number of trials
+
+# %%
+fig, ax = funcSimulation.define_fig_ax()
+
+# 各シミュレーションの結果を保存する変数
+row_label = []
+values = []
+
+# 図のデータをまとめるもの、これを流すことでアニメーションになる
+ims = []
+
+# 一度にstep数simaulateメソッドを使用するシミュレーションを、TRIALの回数行う
+for num in range(TRIAL):
+    np.random.seed(num)
+    O = classSimulation.Simulation(agent_size=0.1, agent=25, 
+                                   view=1, viewing_angle=360, 
+                                   goal_vec=0.06,  
+                                   avoidance='dynamic',
+                                   simple_avoid_vec=0.06, 
+                                   dynamic_avoid_vec=0.06, 
+                                   step=10)
+
+    data = []
+    column_label = []
+    index_label = []
+    index_label.append('initial_value')
+
+    # 出力データの列のラベルを作成
+    for i in range(O.agent):
+        column_label.append('agent ' + str(i) + ' pos_x')
+        column_label.append('agent ' + str(i) + ' pos_y')
+        column_label.append('agent ' + str(i) + ' vel_x')
+        column_label.append('agent ' + str(i) + ' vel_y')
+        column_label.append('agent ' + str(i) + ' collision') # 衝突(距離0以下)
+        column_label.append('agent ' + str(i) + ' half') # 視野の半分の距離まで接近
+        column_label.append('agent ' + str(i) + ' quarter') # 視野の四分の一の距離まで接近
+        column_label.append('agent ' + str(i) + ' one_eighth') # 視野の八分の一の距離まで接近
+
+    # 初期の位置と速度を記録
+    row = []
+    row = np.concatenate([O.all_agent[0]['p'], O.all_agent[0]['v']])
+    # rowにはある時刻の全エージェントの位置と速度が入る
+    for i in range(1, O.agent):
+        row = np.concatenate([row, O.all_agent[i]['p'], O.all_agent[i]['v']])
+
+    # 衝突したエージェントの数を記録
+    collision_agent = O.approach_detect(0)
+    for i in range(O.agent):
+        row = np.append(row, collision_agent[i][1])
+    
+    # 視野の半分より接近したエージェントの数を記録
+    collision_agent = O.approach_detect(0.5)
+    for i in range(O.agent):
+        row = np.append(row, collision_agent[i][1])
+    
+    # 視野の四分の一より接近したエージェントの数を記録
+    collision_agent = O.approach_detect(0.25)
+    for i in range(O.agent):
+        row = np.append(row, collision_agent[i][1])
+        
+    # 視野の八分の一より接近したエージェントの数を記録
+    collision_agent = O.approach_detect(0.125)
+    for i in range(O.agent):
+        row = np.append(row, collision_agent[i][1])
+        
+    # ある時刻でのエージェントの情報が記録されたrowが集まってdataとなる
+    data.append(row)
+
+    # 最初に表示する図の作成
+    plot_data = O.show_image()
+    im = ax.scatter(*plot_data, s=40, marker="o", c='blue')
+    ims.append([im])
+
+    ##### シミュレーション (step数だけ繰り返す) #####
+    for t in tqdm.tqdm(range(O.step)):
+        O.simulate(t + 1)
+        index_label.append(t + 1)
+
+        # シミュレーションごとに値を記録
+        row = []
+        row = np.concatenate([O.all_agent[0]['p'], O.all_agent[0]['v']])
+        for i in range(1, O.agent):
+            row = np.concatenate([row, O.all_agent[i]['p'], O.all_agent[i]['v']])
+
+        # 衝突したエージェントを記録
+        collision_agent = O.approach_detect(0)
+        for i in range(O.agent):
+            row = np.append(row, collision_agent[i][1])
+
+        collision_agent = O.approach_detect(0.5)
+        for i in range(O.agent):
+            row = np.append(row, collision_agent[i][1])
+            
+        collision_agent = O.approach_detect(0.25)
+        for i in range(O.agent):
+            row = np.append(row, collision_agent[i][1])
+
+        collision_agent = O.approach_detect(0.125)
+        for i in range(O.agent):
+            row = np.append(row, collision_agent[i][1])
+
+        data.append(row)
+
+        # 図を作成
+        plot_data = O.show_image()
+        im = ax.scatter(*plot_data, s=40, marker="o", c='blue')
+        ims.append([im])
+        
+        goal_step = O.step
+    ##### シミュレーション終了 ######    
+        
+    # csvとしてステップごとに位置、速度、接近した回数を記録
+    """
+    df = pd.DataFrame(data, columns=column_label, index=index_label)
+    df.to_csv('to_csv_out_' + str(num) + '.csv')
+    """
+    
+    # 最後の座標から完了時間を算出
+    for i in range(O.agent):
+        last_completion_time = O.calc_last_completion_time(i)
+        if not last_completion_time == None:
+            O.completion_time.append(last_completion_time)
+
+    # 完了時間をまとめたファイルを作成
+    column_label = ["completion_time"]
+    index_label = []
+    
+    for i in range(len(O.completion_time)):
+        index_label.append(i + 1)
+    
+    """
+    df = pd.DataFrame(O.completion_time, columns=column_label, index=index_label)
+    df.to_csv('to_csv_out_completion_time_' + str(num) + '.csv')
+    """
+    
+    # 加速度はx, y速度の差からなるベクトルの大きさ
+    accel = []
+    agents_accels = []
+    for i in range(2, 4*O.agent+2, 4):
+        agent_accels = []
+        for j in range(goal_step - 1):
+            # x軸方向の速度の差分
+            x_accel = abs((data[j+1][i] - data[j+2][i]) * 50)
+            # y軸方向の速度の差分
+            y_accel = abs((data[j+1][i+1] - data[j+2][i+1]) * 50)
+            # x, y速度の差からなるベクトルの大きさ
+            temp = np.sqrt(x_accel ** 2 + y_accel ** 2)
+            agent_accels.append(temp)
+            
+        # 全エージェントの加速度を記録
+        agents_accels.append(agent_accels)
+        
+        # 全エージェントの加速度の総和を記録
+        accel.append(np.sum(agent_accels))
+
+        
+    # 加速度をまとめたファイル
+    column_label = []
+    for i in range(O.agent):
+        column_label.append('agent ' + str(i) + ' accel')
+
+    index_label = []
+    for i in range(len(agents_accels[0])):
+        index_label.append(i + 1)
+    
+    df = pd.DataFrame(agents_accels, columns=index_label, index=column_label)
+    df = df.T
+    # df.to_csv('to_csv_out_accel_' + str(num) + '.csv')
+
+   
+    # 衝突した数
+    collision = []
+    for i in range(4*O.agent, 5*O.agent, 1):
+        sum = 0
+        for j in range(goal_step):
+            # 一試行で何回エージェントに衝突したか
+            sum += data[j+1][i]
+        
+        # 全エージェントの衝突した回数を記録
+        collision.append(sum)
+        
+    # 視野の半分に接近したエージェントの数
+    half = []
+    for i in range(5*O.agent, 6*O.agent, 1):
+        sum = 0
+        for j in range(goal_step):
+            sum += data[j+1][i]
+        
+        half.append(sum)
+        
+    # 視野の四分の一に接近した回数
+    quarter = []
+    for i in range(6*O.agent, 7*O.agent, 1):
+        sum = 0
+        for j in range(goal_step):
+            sum += data[j+1][i]
+        
+        quarter.append(sum)
+
+    # 視界の八分の一に接近した回数
+    one_eighth = []
+    for i in range(7*O.agent, 8*O.agent, 1):
+        sum = 0
+        for j in range(goal_step):
+            sum += data[j+1][i]
+        
+        one_eighth.append(sum)
+    
+    # 各指標の平均を計算
+    accel_mean = np.mean(accel)
+    collision_mean = np.mean(collision)
+    half_mean = np.mean(half)
+    quarter_mean = np.mean(quarter)
+    one_eighth_mean = np.mean(one_eighth)
+    completion_time_mean = np.mean(O.completion_time)
+
+    
+    # 各試行の結果のデータを保存
+    row_label.append('seed_' + str(num))
+    values.append([accel_mean, completion_time_mean, half_mean, quarter_mean, 
+                   one_eighth_mean, collision_mean, O.agent, O.viewing_angle, 
+                   O.step, O.avoidance])
+
+# 値をまとめたcsvファイルの作成
+column_label = ['accel', 'time', 'half', 'quarter', 'one_eighth', 'collision', 
+                'agent', 'viewing_angle', 'step', 'method']
+                      
+df = pd.DataFrame(values, columns=column_label, index=row_label)
+# 保存する場所は自由に決めてください
+df.to_csv(f'{O.avoidance}_ang{O.viewing_angle}_agt{O.agent}_stp{O.step}.csv')
+print(df) # show results
+
+# ani = animation.ArtistAnimation(fig, ims, interval=INTERVAL, repeat=False)
+# ani.save(f'ani_{O.avoidance}_ang{O.viewing_angle}_agt{O.agent}_stp{O.step}.mp4')
---
+++ funcSimulation.py
@@ -0,0 +1,207 @@
+""" 
+シミュレーションのための関数 
+2025/01/07
+"""
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import Divider, Size 
+from mpl_toolkits.axes_grid1.mpl_axes import Axes
+
+# %% define figure parameters
+def define_fig_ax(width: int = 500, 
+                  height: int = 500,
+                  SIZE: int = 5) -> plt.subplots:
+    ax_w_px = width  # プロット領域の幅をピクセル単位で指定
+    ax_h_px = height  # プロット領域の高さをピクセル単位で指定
+    
+    #  サイズ指定のための処理、20行目までhttps://qiita.com/code0327/items/43118813b6085dc7e3d1　を参照
+    fig_dpi = 100
+    ax_w_inch = ax_w_px / fig_dpi
+    ax_h_inch = ax_h_px / fig_dpi
+    ax_margin_inch = (0.5, 0.5, 0.5, 0.5)  #  Left, Top, Right, Bottom [inch]
+    
+    fig_w_inch = ax_w_inch + ax_margin_inch[0] + ax_margin_inch[2] 
+    fig_h_inch = ax_h_inch + ax_margin_inch[1] + ax_margin_inch[3]
+    
+    fig = plt.figure(dpi=fig_dpi, figsize=(fig_w_inch, fig_h_inch))
+    ax_p_w = [Size.Fixed(ax_margin_inch[0]), Size.Fixed(ax_w_inch)]
+    ax_p_h = [Size.Fixed(ax_margin_inch[1]), Size.Fixed(ax_h_inch)]
+    divider = Divider(fig, (0.0, 0.0, 1.0, 1.0), ax_p_w, ax_p_h, aspect=False)
+    ax = Axes(fig, divider.get_position())
+    ax.set_axes_locator(divider.new_locator(nx=1, ny=1))
+    fig.add_axes(ax)
+    
+    ax.set_xlim(-SIZE, SIZE)
+    ax.set_ylim(-SIZE, SIZE)
+    ax.grid(True)
+    
+    return fig, ax
+
+# %% functions to calculate the vectors
+def calc_rad(pos2: np.array, # [float, float]
+             pos1: np.array # [float, float]
+             ) -> float: 
+    """
+    pos1からpos2のベクトルの角度を返す
+    ex. calc_rad(pos2=np.array([1.5, 2.5]), pos1=np.array([3.0, 1.0])) -> 2.4
+    """
+    val = np.arctan2(pos2[1] - pos1[1], pos2[0] - pos1[0])
+    
+    return val
+
+
+def rotate_vec(vec: np.array, # [float, float] 
+               rad: float
+               ) -> np.array: # [float, float]
+    """
+    ベクトルをradだけ回転させる (回転行列)
+    ex. rotate_vec(vec=np.array([3.0, 5.0]), rad=1.2) -> array([-3.6, 4.6])
+    """
+    rotation = np.array([[np.cos(rad), -np.sin(rad)], 
+                         [np.sin(rad), np.cos(rad)]])
+    val = np.dot(rotation, vec.T).T
+    
+    return val
+
+
+def calc_distance(posX1, posY1, posX2, posY2):
+    """
+    2点間の距離を計算する
+    ex. calc_distance(3.0, 2.5, 4.5, 5.0) -> 2.9
+    """
+    mypos = np.array([posX1, posY1])
+    anotherpos = np.array([posX2, posY2])
+    distance = np.linalg.norm(mypos - anotherpos)
+    
+    return distance
+
+# %% awareness model
+def calc_cross_point(velx1: float, vely1: float, posx1: float, posy1: float, 
+                     velx2: float, vely2: float, posx2: float, posy2: float
+                     ) -> np.array or None: # [float(x), float(y)]
+    """
+    ex. calc_cross_point(1, 2, 3, 4, 
+                         5, 6, 7, 8) -> array([2.0, 2.0])
+    """
+    nume_x = velx2*vely1*posx1 - velx1*vely2*posx2 + velx1*velx2*(posy2 - posy1)
+    deno_x = velx2*vely1 - velx1*vely2
+    if deno_x == 0:
+        return None
+    else:
+        CPx = nume_x / deno_x    
+
+        CPy = (vely1 / velx1) * (CPx - posx1) + posy1
+        CP = np.array([CPx, CPy])
+        
+        return  CP
+
+
+def calc_deltaTTCP(velx1: float, vely1: float, posx1: float, posy1: float, 
+                   velx2: float, vely2: float, posx2: float, posy2: float
+                   ) -> float or None:
+    """
+    ex. calc_deltaTTCP(-1.4, 0.4, 1.3, -2.1, 
+                       -0.9, -0.3, 2.0, 0.8) -> -2.5
+    """
+    CP = calc_cross_point(velx1, vely1, posx1, posy1, velx2, vely2, posx2, posy2)
+    CPx, CPy = CP[0], CP[1]
+    TTCP0 = TTCP1 = None
+    
+    if (
+            ( (posx1 < CPx and velx1 > 0) or (posx1 > CPx and velx1 < 0) ) 
+        and
+            ( (posy1 < CPy and vely1 > 0) or (posy1 > CPy and vely1 < 0) )
+        ):
+        
+        nume0 = np.sqrt( (CPx - posx1)**2 + (CPy - posy1)**2 )
+        deno0 = np.sqrt( (velx1**2 + vely1**2) )
+        
+        TTCP0 = nume0 / deno0
+    else:
+        return None
+
+    if ( 
+            ( (posx2 < CPx and velx2 > 0) or (posx2 > CPx and velx2 < 0) ) 
+        and
+            ( (posy2 < CPy and vely2 > 0) or (posy2 > CPy and vely2 < 0) )
+        ):
+        
+        nume1 = np.sqrt( (CPx - posx2)**2 + (CPy - posy2)**2 )
+        deno1 = np.sqrt( (velx2**2 + vely2**2) )
+        
+        TTCP1 = nume1 / deno1
+    else:
+        return None
+    
+    deltaTTCP = TTCP0 - TTCP1 #　正は道を譲り、負は自分が先に行く
+    
+    return deltaTTCP    
+    
+
+def calc_angle_two_lines(line1, line2):
+    """
+    2直線がなす角度(radian)を求める関数
+    line1, line2: 各直線を表す2点 [(x1,y1), (x2,y2)] or np.array([[x1,y1], [x2,y2]])
+    
+    ex. calc_angle_two_lines(line1=np.array([[3,　4], [5,　6]]), 
+                             line2=np.array([[-4,　-5], [2, 3]]) -> 0.14
+    """
+    # 傾きを計算
+    def slope(point1, point2):
+        x1, y1 = point1
+        x2, y2 = point2
+        if x2 - x1 == 0:  # 垂直な直線の場合
+            return np.inf  # 無限大を返す
+        return (y2 - y1) / (x2 - x1)
+    
+    m1 = slope(*line1)
+    m2 = slope(*line2)
+    
+    # 垂直チェック
+    if m1 == np.inf and m2 == np.inf:
+        return 0  # 同じ垂直方向
+    elif m1 == np.inf or m2 == np.inf:
+        return np.deg2rad(90) # 片方が垂直の場合(90deg=1.57rad)
+    
+    # 角度を計算
+    angle_rad = np.arctan(np.abs((m2 - m1) / (1 + m1 * m2)))
+    
+    return angle_rad
+
+
+def awareness_model(deltaTTCP, Px, Py, Vself, Vother, theta, Nic):
+    """
+    Inputs
+        deltaTTCP: 自分のTTCPから相手のTTCPを引いた値 (second)
+        Px: 自分から見た相手の相対位置 (x座標m)
+        Py: 自分から見た相手の相対位置 (y座標m)
+        Vself: 自分の歩行速度 (m/s)
+        Vother: 相手の歩行速度 (m/s)
+        theta: 自分の向いている方向と相手の位置の角度差 (rad)
+        Nic: 円内他歩行者数 (人)
+    Output
+        0(注視しない) - 1 (注視する)
+    """
+    if deltaTTCP == None:
+        return 0
+    
+    deno = 1 + np.exp(
+        -(-1.2 +0.018*deltaTTCP -0.1*Px -1.1*Py \
+          -0.25*Vself +0.29*Vother -2.5*theta -0.62*Nic)    
+    )
+    val = 1 / deno
+    
+    return val
+
+def debug_theta(s, num, other):
+    a = s.all_agent[num]['p'] + s.all_agent[num]['v']
+    b = s.all_agent[num]['p']
+    line1 = np.array([b, a])
+    
+    c = s.all_agent[other]['p']
+    line2 = np.array([b, c])
+    theta = calc_angle_two_lines(line1, line2)
+    
+    print('rad:', theta, 'deg:', np.rad2deg(theta))
+    
